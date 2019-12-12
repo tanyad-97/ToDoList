c0bce6cca8ad71d72ff34573ec424727f5a2be5d
Tatiana <tdmitrievskaya@mmtr.ru>
2019-12-12 10:26:38 +0300

    Преобразован цикл for в foreach.Добавлена кнопка "Добавить запись". Добавлена верстка и скрипт для открытия модального окна.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,34 +1,58 @@
 'use strict';
 
-var arrProducts = [
-    {
-        nameProduct: "Dorji bari long Style",
-        price: "$199.99",
-        publData: "2019-10-17"
-    }
-    , {
-        nameProduct: "Dorji  East long Style",
-        price: "$199.99",
-        publData: "2019-10-17"
-    }
-    , {
-        nameProduct: "Dorji bari Style",
-        price: "$199.99",
-        publData: "2019-10-17"
-    }
-    , {
-        nameProduct: "Dorji bari East Style",
-        price: "$199.99",
-        publData: "2019-10-17"
-    }];
-
-let toDoList = document.getElementById('toDoList');
+let list = document.getElementById('list');
 let html = '';
-for (let i = 0; i < arrProducts.length; i++) {
-    const data = arrProducts[i];
-    html+=` <li> ${arrProducts[i].nameProduct}
-            Цена ${arrProducts[i].price}
-            Дата публикации ${arrProducts[i].publData}</li>`;
-}
-toDoList.innerHTML = html;
+let arrProducts = [
+    {
+        name: 'Молоко',
+        edit: false,
+        check: false
+      },
+    {
+        name: 'Хлеб',
+        edit: false,
+        check: false
+      },
+    {
+        name: 'Сыр',
+        edit: false,
+        check: false
+      },
+    {
+        name: 'Чай',
+        edit: false,
+        check: false
+      },
+    {
+        name: 'Фрукты',
+        edit: false,
+        check: false
+      },
+    {
+        name: 'Овощи',
+        edit: false,
+        check: false
+
+    }];
+arrProducts.forEach((item) => {
+    html += ` <li class="list__item"> ${item.name}</li>`;
+});
+
+list.innerHTML = html;
+
+
+var modal = document.querySelector("#modal"),
+    modalOverlay = document.querySelector("#modal-overlay"),
+    closeButton = document.querySelector("#close-button"),
+    openButton = document.querySelector("#open-button");
+
+closeButton.addEventListener("click", function(){
+    modal.classList.toggle("closed");
+    modalOverlay.classList.toggle("closed");
+});
+
+openButton.addEventListener("click", function(){
+    modal.classList.toggle("closed");
+    modalOverlay.classList.toggle("closed");
+});
 
